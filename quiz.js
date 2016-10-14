4b9153c6f652f75945e321e4730038bb80a30007
asimonia <alex.simonian@gmail.com>
2016-10-14 10:50:40 -0500

    Initial commit

---
+++ quiz.js
@@ -0,0 +1,31 @@
+var alex = {
+	height: 10,
+	character: '*'
+}
+
+// the value for the height of the tree should be from user input in a <input type="text">
+// the character to use should be from user input in a <input type="text">
+
+// the user can just press the enter key OR click a button labeled "Grow your tree"
+// the tree should be shown in the console
+// 
+// If either of the input fields does not have a value in it when the user presses the enter key, 
+// or presses the button, then display an alert stating that both fields must have a value.
+// 
+
+/*
+
+	  *
+     ***
+    *****
+   *******
+  *********
+ ***********
+*************
+
+*/
+
+
+function tree(obj) {
+
+}
