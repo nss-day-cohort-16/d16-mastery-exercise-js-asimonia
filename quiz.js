dfaa18e5316dc475cd96001bd80c8588e1570d07
asimonia <alex.simonian@gmail.com>
2016-10-14 12:01:19 -0500

    Progress

--- quiz.js
+++ quiz.js
@@ -1,31 +1,51 @@
-var alex = {
-	height: 10,
-	character: '*'
+// Get references to elements in the DOM
+var growButton = document.getElementById("grow");
+var heightInput = document.getElementById("height");
+var charInput = document.getElementById("char");
+
+// Create a placeholder for a TreeObj object
+var treeObj;
+
+// Create object constructor
+function TreeObj(height, char) {
+	this.height = height;
+	this.char = char;
 }
 
-// the value for the height of the tree should be from user input in a <input type="text">
-// the character to use should be from user input in a <input type="text">
+function growTree() {
+	if (heightInput.value && charInput.value) {
+		alert("You created your tree!");
 
-// the user can just press the enter key OR click a button labeled "Grow your tree"
-// the tree should be shown in the console
-// 
-// If either of the input fields does not have a value in it when the user presses the enter key, 
-// or presses the button, then display an alert stating that both fields must have a value.
-// 
+		// create the Tree object
+		treeObj = new TreeObj(heightInput.value, charInput.value);
+		
+		// call the tree function and pass the object
+		tree(treeObj);
+		
+	} else {
+		alert("Both fields must have a value");
+	}
+}
 
-/*
+// Add event listeners
+growButton.addEventListener("click", function(event) {
+	event.preventDefault();
+	growTree();
+});
 
-	  *
-     ***
-    *****
-   *******
-  *********
- ***********
-*************
+heightInput.addEventListener("keydown", function(event) {
+	if (event.keyCode == 13) {
+		growTree();
+	}
+});
 
-*/
+charInput.addEventListener("keydown", function(event) {
+	if (event.keyCode == 13) {
+		growTree();
+	}
+});
 
 
 function tree(obj) {
-
+	console.log(obj);
 }
